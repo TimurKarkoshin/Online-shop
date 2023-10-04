1ef99b8b5788549af91aae53b94acd73b17a973f
Kamyshin Ivan <ikamyshin@yandex.ru>
2023-10-04 19:13:50 +0300

    Передача разработки функционала

--- market/config/urls.py
+++ market/config/urls.py
@@ -15,19 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from config import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path("", include("shops.urls")),  # главная страница
-    path("admin/", admin.site.urls),
-    path("profile/", include("profiles.urls")),
-    path("products/", include("products.urls")),
-    path("i18n", include("django.conf.urls.i18n")),
-    path("basket/", include("basket.urls")),
-    path("history/", include("history.urls")),
-    path("order/", include("order.urls")),
-]
+urlpatterns = i18n_patterns(
+    [
+        path("", include("shops.urls")),
+        path("admin/", admin.site.urls),
+        path("profile/", include("profiles.urls")),
+        path("products/", include("products.urls")),
+        path("basket/", include("basket.urls")),
+        path("history/", include("history.urls")),
+        path("order/", include("order.urls")),
+    ]
+)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
